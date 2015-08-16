0b4314272ca866896dc5614982be26cf49b67aa3
mctep <mctep77@gmail.com>
2015-08-16 17:01:58 +0000

    some fixes

--- server/config.js
+++ server/config.js
@@ -2,5 +2,9 @@ var config = module.exports = {};
 
 config.PORT = 3300;
 config.HOST = '0.0.0.0';
-config.LIVERELOAD_PORT = 3301;
+
 config.ENVIRONMENT = process.env.NODE_ENV || 'development';
+
+if (config.ENVIRONMENT == 'development') {
+	config.LIVERELOAD_PORT = 3301;
+}
